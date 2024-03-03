7f866a661930668263697c1428639a1c91a28feb
Nabham <nnssuser@gmail.com>
2024-03-03 18:36:21 +0530

    Redux handler added

--- src/components/Counter/Button.jsx
+++ src/components/Counter/Button.jsx
@@ -1,6 +1,8 @@
-const Button =({children})=>{
+const Button =({children,onClick})=>{
 return (
-    <button style={{backgroundColor:"purple",color:'white',borderRadius:"10px",marginRight:"30px",marginLeft:"30px",marginTop:"30px"}}>{children}</button>
+    <button style={{backgroundColor:"purple",color:'white',borderRadius:"10px",marginRight:"30px",marginLeft:"30px",marginTop:"30px"}}
+    onClick={onClick} 
+    >{children}</button>
 )
 }
 
